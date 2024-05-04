2fa6eb57718535407b245c96af58c057dea82329
Nathaniel Rivera <52226518+DeveloperSpoot@users.noreply.github.com>
2024-05-04 05:22:46 -0700

    Completed Person and Customer classes. Still need to create demo app for it.

---
+++ PersonCustomer/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace CS162_Chapter12_ProgrammingProblems.PersonCustomer
+{
+    public class Customer:Person
+    {
+        public bool MailingList { get; set; }
+
+        public Customer() : base()
+        {
+            MailingList = false;
+        }
+
+        public Customer(string name, string address, int phone, bool mailList):base(name, address, phone)
+        {
+            MailingList = mailList;
+        }
+    }
+}
---
+++ PersonCustomer/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace CS162_Chapter12_ProgrammingProblems.PersonCustomer
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Phone { get; set; }
+
+        public Person()
+        {
+            Name = "";
+            Address = "";
+            Phone = 0;
+        }
+
+        public Person(string name, string address, int phone)
+        {
+            Name = name;
+            Address = address;
+            Phone = phone;
+        }
+    }
+}
