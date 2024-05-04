590425a2eed9b14e0db6911e00fd1515d8201051
Nathaniel Rivera <52226518+DeveloperSpoot@users.noreply.github.com>
2024-05-04 05:37:20 -0700

    Perffered Customer Class mostly completed.

---
+++ PersonCustomer/PerfferedCustomers.cs
@@ -0,0 +1,24 @@
+﻿namespace CS162_Chapter12_ProgrammingProblems.PersonCustomer
+{
+    public class PerfferedCustomers:Customer
+    {
+        decimal PurchasedTotal { get; set; }
+        decimal DiscountLevel { get; set; }
+
+        public PerfferedCustomers() : base()
+        {
+            PurchasedTotal = 0m;
+            DiscountLevel = 0;
+        }
+
+        public PerfferedCustomers(string name, string address, int phone, bool mailList, decimal purchased, decimal discountLevel):base(name, address, phone, mailList)
+        {
+            PurchasedTotal = purchased;
+            DiscountLevel = discountLevel;
+        } 
+        private decimal getDiscountLevel()
+        {
+            return 0m;
+        }
+    }
+}
